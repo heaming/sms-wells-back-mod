109cba51b22235c54548e1e17c0fa0862b2efefa
Jaewoo Jung <gs.piit15@kyowon.co.kr>
2022-10-30 15:05:39 +0900

    feat: 조직 샘플 API Response 결과 데이터 추가

--- src/main/java/com/kyowon/sms/wells/web/organization/sample/rest/EozwSampleController.java
+++ src/main/java/com/kyowon/sms/wells/web/organization/sample/rest/EozwSampleController.java
@@ -19,6 +19,6 @@ public class EozwSampleController {
     @ApiOperation(value = "조직 샘플목록 조회", notes = "파라미터 없이 샘플데이터 조회한다.")
     @GetMapping
     public List<String> getSampleData() {
-        return List.of("TEST1", "TEST2");
+        return List.of("TEST1", "TEST2", "TEST3");
     }
 }
