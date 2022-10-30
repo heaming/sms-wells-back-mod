105fe8320c083c61ee38458e9231cb3d1bcc7e04
정재우 <gs.piit15@kyowon.co.kr>
2022-10-30 15:56:54 +0900

    feat: 조직 샘플 API 로그 추가 및 결과 데이터 변경

--- src/main/java/com/kyowon/sms/wells/web/organization/sample/rest/EozwSampleController.java
+++ src/main/java/com/kyowon/sms/wells/web/organization/sample/rest/EozwSampleController.java
@@ -19,6 +19,7 @@ public class EozwSampleController {
     @ApiOperation(value = "조직 샘플목록 조회", notes = "파라미터 없이 샘플데이터 조회한다.")
     @GetMapping
     public List<String> getSampleData() {
-        return List.of("TEST1", "TEST2");
+        System.out.println("===> TEST LOG");
+        return List.of("TEST1", "TEST2", "TEST3", "TEST4");
     }
 }
