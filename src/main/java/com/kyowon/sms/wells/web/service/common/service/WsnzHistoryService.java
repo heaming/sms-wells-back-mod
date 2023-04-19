d41775b485d7c6349f57bd210d54cba7b3cd1561
juno.cha <gs.piit134@kyowon.com>
2023-04-19 17:24:24 +0900

    fix : History 로직 추가 및 수정(고객서비스 로직 후처리 로그 모듈 개발)

---
+++ src/main/java/com/kyowon/sms/wells/web/service/common/service/WsnzHistoryService.java
@@ -0,0 +1,126 @@
+package com.kyowon.sms.wells.web.service.common.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kyowon.sms.wells.web.service.common.mapper.WsnzHistoryMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class WsnzHistoryService {
+
+    private final WsnzHistoryMapper mapper;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    /**
+     *
+     * @param vo 변환할 Source Value Object
+     * @return Map 변환된 Target Value Object(Map)
+     */
+    public Map convertVoToMap(Object vo){
+        Map map = new HashMap<String, String>();
+        map = objectMapper.convertValue(vo, Map.class);
+        return map;
+    }
+
+    /**
+     * 고객서비스BS배정이력 History Log
+     * (TB_SVPD_CST_SV_BFSVC_ASN_HIST)
+     * @param vo
+     * cstSvAsnNo
+     * asnOjYm
+     * cntrNo
+     * cntrSn
+     * svBizMclsfCd
+     * svBizDclsfCd
+     * wkSn
+     * asnPsicMngtDvCd
+     * asnPsicDvCd
+     * asnPsicPrtnrOgTpCd
+     * asnPsicPrtnrNo
+     * cnfmPsicMngtDvCd
+     * cnfmPsicAsnDt
+     * cnfmPsicAsnHh
+     * cnfmPsicDvCd
+     * cnfmPsicPrtnrOgTpCd
+     * cnfmPsicPrtnrNo
+     * egerPstnCd
+     * egerRsbCd
+     * vstCnfmdt
+     * vstCnfmHh
+     * vstOjLocaraCd
+     * mngerRglvlDvCd
+     * lstmmCrdovrYn
+     * lstmmVstCnfmdt
+     * lstmmVstCnfmHh
+     * vstDuedt
+     * vstExpHh
+     * vstPrgsStatCd
+     * wkExcnDt
+     * wkCanRsonCd
+     * asnTfDvCd
+     * siteAwPdGrpCd
+     * siteAwSvTpCd
+     * siteAwAtcCd
+     * cstCntcDt
+     * cntcIchrPrtnrOgTpCd
+     * cntcIchrPrtnrNo
+     * cstCntcCd
+     * cstCntcMoCn
+     * spcAsTpCd
+     * hpcallFwYn
+     * ichrLocaraDvCd
+     * arvDt
+     * arvHh
+     * wkGrpCd
+     * locaraGdCd
+     * locaraChaosGdCd
+     * dtaDlYn
+     *
+     * @return
+     */
+    public int insertCstSvBfsvcAsnHistByMap(Object vo){
+        return mapper.insertCstSvBfsvcAsnHistByMap(this.convertVoToMap(vo));
+    }
+
+    /**
+     * 고객서비스BS대상이력 History Log
+     * (TB_SVPD_CST_SV_BFSVC_OJ_HIST)
+     * @param vo
+     * cntrNo
+     * asnOjYm
+     * cntrSn
+     * svBizMclsfCd
+     * svBizDclsfCd
+     * wkSn
+     * mngrDvCd
+     * rltdSvBizDclsfCd
+     * clsfCdSrnPrntCn
+     * adrId
+     * lstmmCrdovrYn
+     * bfVstDuedt
+     * pdctPdCd
+     * istNmnN
+     * vstNmnN
+     * egerWkYn
+     * vstDuedt
+     * asRefriDvCd
+     * bfsvcRefriDvCd
+     * vstDvCd
+     * mtrStatCd
+     * spcAsTpCd
+     * cstSvAsnNo
+     * dtaDlYn
+     *
+     * @return
+     */
+    public int insertCstBfsvcOjHistByMap(Object vo){
+        return mapper.insertCstBfsvcOjHistByMap(this.convertVoToMap(vo));
+    }
+
+}
