8eddc46d00183795c43d88ba74e8b42d1e62320d
inho.choi <gs.piit181@kyowon.co.kr>
2023-04-27 10:00:56 +0900

    fix: 삭제가 아닌 수정으로 method 수정

--- src/main/java/com/kyowon/sms/wells/web/service/stock/rest/WsnaAutomaticOstrAkNpDlController.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/rest/WsnaAutomaticOstrAkNpDlController.java
@@ -1,6 +1,6 @@
 package com.kyowon.sms.wells.web.service.stock.rest;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +13,12 @@ import lombok.RequiredArgsConstructor;
 @Api(tags = "[WSNA] 자동출고 요청건 중 미처리건 삭제 처리 REST API")
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(SnServiceConst.REST_URL_V1 + "/auto-out-strock-untreated")
+@RequestMapping(SnServiceConst.REST_URL_V1 + "/automatic-ostr-ak-np-dl")
 public class WsnaAutomaticOstrAkNpDlController {
 
     private final WsnaAutomaticOstrAkNpDlService service;
 
-    @DeleteMapping
+    @PutMapping
     public int removeAutomaticOstrAkNpDls() {
         return service.removeAutoOstrAkNpDls();
     }
