7f92eb5af3fb2d39d77ccb4fd03674ee71cf7267
inho.choi <gs.piit181@kyowon.co.kr>
2023-05-26 10:57:53 +0900

    feat: mapper명 오류 수정

--- src/main/java/com/kyowon/sms/wells/web/service/stock/service/WsnaIndpStockBldTfService.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/service/WsnaIndpStockBldTfService.java
@@ -5,12 +5,14 @@ import com.sds.sflex.system.config.response.SaveResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class WsnaIndpStockBldTfService {
     private final WsnaIndpStockBldTfMapper mapper;
+    @Transactional
     public int SaveCarriedOverAddressUseYn(){
         return mapper.updateCarriedOverAddressUseYn();
     }
