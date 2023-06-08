ffbd991e107cadaf9ce14ed46efb1d40b71e368b
김새롬이 <gs.piit238@kyowon.co.kr>
2023-06-08 09:53:16 +0900

    chore : 물류 배송요청 (HQ생성) 로직 수정
    * 명칭 관련 SQL 수정
    * 주문번호 업데이트 처리 로직 수정

--- src/main/java/com/kyowon/sms/wells/web/service/stock/service/WsnaLogisticsDeliveryAskService.java
+++ src/main/java/com/kyowon/sms/wells/web/service/stock/service/WsnaLogisticsDeliveryAskService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
@@ -43,29 +44,38 @@ public class WsnaLogisticsDeliveryAskService {
         List<WsnaLogisticsDeliveryAskDto.CreateReq> dtos
     ) {
 
+        // TB_IFIN_SPP_BAS_SEND_ETXT - 배송기본송신전문 데이터 생성 건수
+        int basCnt = 0;
+        // TB_IFIN_SPP_PD_SEND_ETXT - 배송상품송신전문 데이터 생성 건수
+        int pdCnt = 0;
+        // TB_IFIN_SPP_MAT_SEND_ETXT - 배송자재송신전문 데이터 생성 건수
+        int matCnt = 0;
+
         // TB_IFIN_KSS_QOM_ASN_SEND_TEMP - KSS물량배정송신전문 데이터 생성
         this.mapper.insertKssQomAsnSendTemp(dtos);
 
         // 파트너번호별 주문번호 조회
         List<WsnaLogisticsDeliveryAskBssDvo> bssDvos = this.mapper.selectKssQomAsnSendTempLlornos();
 
-        // 주문번호 업데이트
-        this.mapper.updateKssQomAsnSendTempLlornos(bssDvos);
+        if (CollectionUtils.isNotEmpty(bssDvos)) {
+            // 주문번호 업데이트
+            this.mapper.updateKssQomAsnSendTempLlornos(bssDvos);
 
-        // TB_IFIN_SPP_BAS_SEND_ETXT - 배송기본송신전문 데이터 생성
-        int basCnt = this.mapper.insertSppBasSendEtxt();
+            // TB_IFIN_SPP_BAS_SEND_ETXT - 배송기본송신전문 데이터 생성
+            basCnt = this.mapper.insertSppBasSendEtxt();
 
-        // 차수
-        int tcnt = dtos.get(0).tcnt();
+            // 차수
+            int tcnt = dtos.get(0).tcnt();
 
-        // TB_IFIN_KSS_QOM_ASN_ITM_TEMP - KSS물량배정품목임시 데이터 생성
-        this.mapper.insertKssQomAsnItmTemp(tcnt);
+            // TB_IFIN_KSS_QOM_ASN_ITM_TEMP - KSS물량배정품목임시 데이터 생성
+            this.mapper.insertKssQomAsnItmTemp(tcnt);
 
-        // TB_IFIN_SPP_PD_SEND_ETXT - 배송상품송신전문 데이터 생성
-        int pdCnt = this.mapper.insertSppPdSendEtxt();
+            // TB_IFIN_SPP_PD_SEND_ETXT - 배송상품송신전문 데이터 생성
+            pdCnt = this.mapper.insertSppPdSendEtxt();
 
-        // TB_IFIN_SPP_MAT_SEND_ETXT - 배송자재송신전문
-        int matCnt = this.mapper.insertSppMatSendEtxt();
+            // TB_IFIN_SPP_MAT_SEND_ETXT - 배송자재송신전문 데이터 생성
+            matCnt = this.mapper.insertSppMatSendEtxt();
+        }
 
         return new WsnaLogisticsDeliveryAskDto.CreateRes(basCnt, pdCnt, matCnt);
     }
