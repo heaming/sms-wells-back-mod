8715a9d32d0b515910bc4e9d1e8ac4c4da2100be
gs.piit122 <kdycorp@kyowon.co.kr>
2023-06-26 17:42:29 +0900

    fix: 타임테이블

--- src/main/java/com/kyowon/sms/wells/web/service/allocate/mapper/WsncTimeTableMapper.java
+++ src/main/java/com/kyowon/sms/wells/web/service/allocate/mapper/WsncTimeTableMapper.java
@@ -6,8 +6,6 @@ import java.util.Optional;
 import com.kyowon.sms.wells.web.service.allocate.dvo.*;
 import org.apache.ibatis.annotations.Mapper;
 
-import com.kyowon.sms.wells.web.service.allocate.dto.WsncTimeTableSalesDto;
-
 /**
  *
  *
@@ -31,7 +29,7 @@ public interface WsncTimeTableMapper {
     //WsncTimeTablePackageDvo selectPackage(String sidingCd, String baePdCd, String cntrNo);
     Optional<WsncTimeTableCntrDvo> selectContract(String cntrNo, String cntrSn, String sellDate);
 
-    WsncTimeTableProductDvo selectProduct(String basePdCd, String pdctPdCd);
+    Optional<WsncTimeTableProductDvo> selectProduct(String basePdCd, String pdctPdCd);
 
     //String selectOjPdCd(String saleCd);
 
@@ -41,7 +39,7 @@ public interface WsncTimeTableMapper {
 
     //List<WsncTimeTableMonthScheduleDvo> selectMonthSchedule(String prtnrNo);
 
-    List<WsncTimeTableDisableDaysDvo> selectDisableDays(WsncTimeTableSalesParamDvo dvo);
+    List<WsncTimeTableDisableDaysDvo> selectDisableDays(WsncTimeTableParamDvo dvo);
 
     List<WsncTimeTableSidingDaysDvo> selectSidingDaysForSpay(
         String lcst09,
@@ -52,6 +50,8 @@ public interface WsncTimeTableMapper {
         String cntrNo
     );
 
+    List<WsncTimeTableMonthScheduleDvo>selectMonthSchedule(String prtnrNo);
+
     String selectFnSvpdLocaraPrtnr01(String newAdrZip, String pdctPdCd, String svBizDclsfCd, String sellDate);
 
     String selectFnSvpdLocaraPrtnrOwr01(String newAdrZip, String pdctPdCd, String svBizDclsfCd, String sellDate);
@@ -60,7 +60,7 @@ public interface WsncTimeTableMapper {
         String newAdrZip, String pdctPdCd, String svBizDclsfCd, String sellDate, String vstGb
     );
 
-    Optional<WsncTimeTableRpbLocaraPsicDvo> selectRpbLocaraPsic(WsncTimeTableSalesParamDvo dvo);
+    Optional<WsncTimeTableRpbLocaraPsicDvo> selectRpbLocaraPsic(WsncTimeTableParamDvo dvo);
 
     WsncTimeTablePsicDataDvo selectPsicData(WsncTimeTableRpbLocaraPsicDvo dvo);
 
@@ -70,5 +70,5 @@ public interface WsncTimeTableMapper {
     String selectDegWrkCnt(WsncTimeTableRpbLocaraPsicDvo dvo);
     String selectWkHhCd(WsncTimeTableRpbLocaraPsicDvo dvo);
 
-    List<String> selectOffDays(WsncTimeTableSalesParamDvo dvo);
+    List<String> selectOffDays(WsncTimeTableParamDvo dvo);
 }
