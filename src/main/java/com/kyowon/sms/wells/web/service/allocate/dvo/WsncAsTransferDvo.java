9cb11ecc61f111d15ebf635036be2b34fc4bf746
gs.piit129 <gs.piit129@kyowon.co.kr>
2023-07-04 17:14:12 +0900

    fix : a/s 담당자 이관 저장 로직 수정

--- src/main/java/com/kyowon/sms/wells/web/service/allocate/dvo/WsncAsTransferDvo.java
+++ src/main/java/com/kyowon/sms/wells/web/service/allocate/dvo/WsncAsTransferDvo.java
@@ -47,24 +47,17 @@ public class WsncAsTransferDvo {
     String bfchEmpno; /* 사번(이관전담당자) */
     String bfchFnm; /* 성명(이관전담당자) */
     String afchBlgCdOrigin; /* 소속ID(이관후담당자)origin */
+    String afchOgTpCdOrigin;
     String afchEmpnoOrigin; /* 소속명(이관후담당자)origin */
+    String afchFnmOrigin;
     String afchBlgCd; /* 소속ID(이관후담당자) */
-    String afchBlgNm; /* 소속명(이관후담당자) */
+    String afchOgTpCd;
     String afchEmpno; /* 사번(이관후담당자) */
-    String afchFnm; /* 성명(이관후담당자) */
+    String afchFnm;
     String tfDt; /* 이관일자(이관요청정보) */
     String tfRsonNm; /* 이관사유(이관요청정보) */
     String tfBlgNm; /* 소속(이관요청정보) */
     String tfFnm; /* 성명(이관요청정보) */
-
-    //    String cstSvAsnNo; /* 고객서비스배정번호 */
-    //    String cntrNo; /* 계약번호 */
-    //    String cntrSn; /* 계약일련번호 */
-    //    String basePdCd; /* 상품코드 */
-    //    String afchBlgCdOrigin; /* 소속ID(변경전) */
-    //    String afchEmpnoOrigin; /* 파트너번호(변경전) */
-    //    String afchBlgCd; /* 소속ID(변경후) */
-    //    String afchEmpno; /* 파트너번호(변경후) */
     String sppOrdNo; /* 배송주문번호 */
     String sppPlanSn; /* 배송계획일련번호 */
 
--- src/main/java/com/kyowon/sms/wells/web/service/allocate/service/WsncAsTransferService.java
+++ src/main/java/com/kyowon/sms/wells/web/service/allocate/service/WsncAsTransferService.java
@@ -6,9 +6,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.kyowon.sms.wells.web.service.allocate.converter.WsncAsTransferConverter;
-import com.kyowon.sms.wells.web.service.allocate.dto.WsncAsTransferDto.*;
+import com.kyowon.sms.wells.web.service.allocate.dto.WsncAsTransferDto.SaveReq;
+import com.kyowon.sms.wells.web.service.allocate.dto.WsncAsTransferDto.SearchReq;
+import com.kyowon.sms.wells.web.service.allocate.dto.WsncAsTransferDto.SearchRes;
 import com.kyowon.sms.wells.web.service.allocate.dvo.WsncAsTransferDvo;
 import com.kyowon.sms.wells.web.service.allocate.mapper.WsncAsTransferMapper;
+import com.kyowon.sms.wells.web.service.common.mapper.WsnzHistoryMapper;
 import com.sds.sflex.system.config.datasource.PageInfo;
 import com.sds.sflex.system.config.datasource.PagingResult;
 
@@ -19,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class WsncAsTransferService {
     private final WsncAsTransferMapper mapper;
     private final WsncAsTransferConverter converter;
+    private final WsnzHistoryMapper wsnzHistoryMapper;
 
     /**
      * A/S 담당자 이관
@@ -57,23 +61,44 @@ public class WsncAsTransferService {
      * @return
      */
     @Transactional
-    public int saveAsTransfers(List<SaveReq> dtos) {
+    public int saveAsTransfers(List<SaveReq> dtos) throws Exception {
         int processCount = 0;
         for (SaveReq dto : dtos) {
             WsncAsTransferDvo dvo = converter.mapSaveReqToWsncAsTransferDvo(dto);
-            mapper.insertAssignResultTransferIz(dvo);
-            mapper.updateInstallationAssignIz(dvo);
-            mapper.updateSeedingShippingPlanIz(dvo);
-            mapper.updateSeedingShippingPlanIz(dvo);
+            WsncAsTransferDvo dvo1 = dvo;
+            processCount += mapper.insertAssignResultTransferIz(dvo);
+
+            //이관출고로그저장
+            wsnzHistoryMapper.insertCstSvasIstAsnHistByPk(dvo.getCstSvAsnNo());
+            Thread.sleep(1000);
+            processCount += mapper.updateInstallationAssignIz(dvo);
+            //이관입고로그저장
+            wsnzHistoryMapper.insertCstSvasIstAsnHistByPk(dvo.getCstSvAsnNo());
+            //웰스팜 기기- 모종 연계코드가 존재하는지여부
+            if (mapper.selectFarmCodeCount(dvo) > 0) {
+                // 해당 모종 고객번호로 배정된 설치/AS가 있는지 확인
+                if (mapper.selectAssignByPk(dvo) > 0) {
+                    // TB_SVPD_SDING_SPP_PLAN_IZ 업데이트위한 조회(List)
+                    List<WsncAsTransferDvo> farmDvos = mapper.selectFarmShippings(dvo);
+                    for (WsncAsTransferDvo farmDvo : farmDvos) {
+                        dvo1.setCstSvAsnNo(farmDvo.getCstSvAsnNo());
+                        dvo1.setCntrNo(farmDvo.getCntrNo());
+                        dvo1.setCntrSn(farmDvo.getCntrSn());
+
+                        //이관처리
+                        mapper.insertAssignResultTransferIz(dvo1);
+                    }
+                    // TODO: 웰스팜 3개월 매니저, 모종 2개월 택배발송 출시에 따른 수정 픽업.
+                    //                    if(){
+                    //                        processCount += mapper.updateInstallationAssignIz(dvo1);
+                    //                        processCount += mapper.updateSeedingShippingPlanIz(dvo1);
+                    //                    }
+
+                }
+            }
+
         }
         return processCount;
     }
 
-    public List<Engineer> getEngineers(FindEngineerReq dto) {
-        return mapper.selectEngineers(dto);
-    }
-
-    public List<Center> getCenters() {
-        return mapper.selectCenters();
-    }
 }
