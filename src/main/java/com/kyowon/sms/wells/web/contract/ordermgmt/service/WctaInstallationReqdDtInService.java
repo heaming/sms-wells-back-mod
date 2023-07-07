4bc344b7f63c381c60fa0d26f81106c868c7bb58
Alex Cho <gs.piit8@kyowon.com>
2023-07-07 19:49:21 +0900

    판매 설치일자 UPDATE 서비스 예정일자 추가

--- src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/service/WctaInstallationReqdDtInService.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/service/WctaInstallationReqdDtInService.java
@@ -25,7 +25,7 @@ public class WctaInstallationReqdDtInService {
      *        reqdDt  철수일자
      * @return
      */
-    public int saveInstallReqdDt(String cntrNo, String cntrSn, String istDt, String reqdDt) {
+    public int saveInstallReqdDt(String cntrNo, String cntrSn, String istDt, String reqdDt, String sppDuedt) {
         ValidAssert.hasText(cntrNo);
         ValidAssert.hasText(cntrSn);
         BizAssert.isFalse(StringUtils.isAllEmpty(istDt, reqdDt), "MSG_ALT_NO_APPY_TARGET_DATA");
@@ -36,20 +36,42 @@ public class WctaInstallationReqdDtInService {
             mapper.updateContractWellsDetailHist(cntrNo, cntrSn);
             mapper.insertContractWellsDetailHist(cntrNo, cntrSn);
         }
-        /* 설치일자가 입력되었다면 계약상세의 계약상품시작일자를 update한다. */
-        if (resultCount > 0 && !istDt.isEmpty()) {
-            int resultCount2= mapper.updateContractDetailPdStrtdt(cntrNo, cntrSn);
 
-            if (resultCount2 > 0) {
-                mapper.updateContractDetailHist(cntrNo, cntrSn);
-                mapper.insertContractDetailHist(cntrNo, cntrSn);
-            }
+        int resultCount2 = 0, resultCount3 = 0;
+        /* 예정일자가 있는 경우, 예정일자를 UPDATE */
+        if (StringUtils.isNotEmpty(sppDuedt)) {
+            resultCount2 = mapper.updateInstallOrderReqDt( cntrNo, cntrSn, sppDuedt) ;
+        }
+
+        /* 설치일자가 입력되었거나, 예정일자 계약상세의 계약상품시작일자를 update한다. */
+        if ((resultCount > 0 && !istDt.isEmpty()) || resultCount2 > 0 ) {
+            resultCount2= mapper.updateContractDetailPdStrtdt(cntrNo, cntrSn);
+        }
+
+        if (resultCount2 > 0 || resultCount3 > 0) {
+            mapper.updateContractDetailHist(cntrNo, cntrSn);
+            mapper.insertContractDetailHist(cntrNo, cntrSn);
         }
 
         return resultCount;
     }
 
     public int saveInstallOrderReqDt(String cntrNo, String cntrSn, String sppDuedt) {
+        ValidAssert.hasText(cntrNo);
+        ValidAssert.hasText(cntrSn);
+        BizAssert.isFalse(StringUtils.isEmpty(sppDuedt), "MSG_ALT_NO_APPY_TARGET_DATA");
+
+        int resultCount = mapper.updateInstallOrderReqDt( cntrNo, cntrSn, sppDuedt) ;
+
+        if(resultCount > 0){
+            mapper.updateContractDetailHist(cntrNo, cntrSn);
+            mapper.insertContractDetailHist(cntrNo, cntrSn);
+        }
+
+        return resultCount;
+    }
+
+    public int saveDeliveryConfirm(String cntrNo, String cntrSn, String sppDuedt) {
         ValidAssert.hasText(cntrNo);
         ValidAssert.hasText(cntrSn);
         BizAssert.isFalse(StringUtils.isEmpty(sppDuedt), "MSG_ALT_NO_APPY_TARGET_DATA");
