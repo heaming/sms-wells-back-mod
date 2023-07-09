7a9d98e05433357d4e12ec8dad68a07e44201822
Jaewoo Jung <gs.piit15@kyowon.co.kr>
2023-07-09 14:45:42 +0900

    fix: interfaceController로 변경  + RequestMapping 추가

--- src/main/java/com/kyowon/sms/wells/web/service/interfaces/rest/WsnbWorkOrderInterfaceController.java
+++ src/main/java/com/kyowon/sms/wells/web/service/interfaces/rest/WsnbWorkOrderInterfaceController.java
@@ -5,21 +5,22 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.kyowon.sms.wells.web.service.interfaces.dto.WsnbWorkOrderInterfaceDto.CreateOrderReq;
 import com.kyowon.sms.wells.web.service.interfaces.dto.WsnbWorkOrderInterfaceDto.CreateOrderRes;
 import com.kyowon.sms.wells.web.service.interfaces.service.WsnbWorkOrderInterfaceService;
 import com.kyowon.sms.wells.web.service.zcommon.constants.SnServiceConst;
+import com.sds.sflex.system.config.annotation.InterfaceController;
 import com.sds.sflex.system.config.webclient.ivo.EaiWrapper;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 
-@RestController
+@InterfaceController
 @Api(tags = SnServiceConst.REST_INTERFACE_DOC_V1 + ": A/S, 분리, 재설치 및 설치정보 변경 등록")
 @RequestMapping(SnServiceConst.REST_INTERFACE_URL_V1 + "/work-orders")
 @RequiredArgsConstructor
@@ -29,6 +30,7 @@ public class WsnbWorkOrderInterfaceController {
     private final WsnbWorkOrderInterfaceService service;
 
     @ApiOperation(value = "A/S, 분리, 재설치 및 설치정보 변경 등록 작업 오더 생성", notes = "타시스템(교원웰스, 고객센터, KMEMBERS)에서 다건의 A/S, 분리, 재설치 서비스 작업 오더 생성을 위해 사용한다.")
+    @PostMapping
     public EaiWrapper createMultipleTaskOrders(
         @Valid
         @RequestBody
