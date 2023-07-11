4c9337efe21c842a95a9849fd7a48264d2fff7b4
domin.pyun <gs.piit203@kyowon.co.kr>
2023-07-11 09:36:16 +0900

    chore : 불필요한 상수 제거

--- src/main/java/com/kyowon/sms/wells/web/promotion/zcommon/constants/PmPromotionConst.java
+++ src/main/java/com/kyowon/sms/wells/web/promotion/zcommon/constants/PmPromotionConst.java
@@ -31,8 +31,6 @@ public class PmPromotionConst {
     public static final String SYS_CMPP_NM_STANDARD_FEE_AMOUNT = "stdFeeAmt";       // (시스템항목명) 기준수수료
     public static final String SYS_CMPP_NM_FEE_FIX_AMOUNT_YN = "feeFxamYn";         // (시스템항목명) 수수료정액여부
     public static final String SYS_CMPP_NM_PREPAY_DUP_PERMIT_YN = "prmDupPrmitYn";  // (시스템항목명) 선납중복허용여부
-    public static final String SYS_CMPP_NM_FREE_GIFT_CHOICE_GROUP = "fgptChoGrpCd"; // (시스템항목명) 사은품선택그룹
-    public static final String SYS_CMPP_NM_FREE_GIFT_CHOICE = "fgptChoCd";          // (시스템항목명) 사은품선택
     public static final String SYS_CMPP_NM_FREE_GIFT = "fgptCd";                    // (시스템항목명) 사은품상품코드
     public static final String SYS_CMPP_NM_FREE_GIFT_QUANTITY = "fgptQty";          // (시스템항목명) 사은품수량
     public static final String SYS_CMPP_NM_APPLY_GROUP_CODE = "pmotApyGrpCd";       // (시스템항목명) 프로모션적용그룹
