1fdfb29cc187759a0a738b3de9d276d958a86853
gs.piit238 <gs.piit238@kyowon.co.kr>
2023-07-27 10:08:10 +0900

    chore : 독립 창고 예외 빌딩 지역 데이터 이관 삭제
    * 배치로 이관

--- src/main/java/com/kyowon/sms/wells/web/service/stock/mapper/WsnaIndpStockBuildingTransferMapper.java
+++
@@ -1,8 +0,0 @@
-package com.kyowon.sms.wells.web.service.stock.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface WsnaIndpStockBuildingTransferMapper {
-    int updateCarriedOverAddressUseYn();
-}
