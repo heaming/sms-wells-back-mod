410be8ca7647d72004e940fc9324d189afa58d49
younuk.choi <gs.piit186@kyowon.co.kr>
2023-08-02 16:23:14 +0900

    feat : 최사설치 조회. 등록. 엑셀업로드 - 신규개발

--- src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/converter/WctaCompanyInstallConverter.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/converter/WctaCompanyInstallConverter.java
@@ -1,11 +1,60 @@
 package com.kyowon.sms.wells.web.contract.ordermgmt.converter;
 
-import org.mapstruct.Mapper;
+import java.util.List;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.kyowon.sms.wells.web.contract.interfaces.dvo.WctiContractCreateDvo;
+import com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaCompanyInstallDto.SaveReq;
 import com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaCompanyInstallDto.SearchReq;
+import com.kyowon.sms.wells.web.contract.ordermgmt.dto.WctaCompanyInstallDto.SearchRes;
 import com.kyowon.sms.wells.web.contract.ordermgmt.dvo.WctaCompanyInstallDvo;
+import com.kyowon.sms.wells.web.contract.ordermgmt.dvo.WctaContractAdrpcBasDvo;
+import com.sds.sflex.system.config.datasource.PagingResult;
 
 @Mapper(componentModel = "spring")
 public interface WctaCompanyInstallConverter {
     WctaCompanyInstallDvo mapSearchReqToDvo(SearchReq dto);
+
+    List<SearchRes> mapAllDvoToSearchRes(List<WctaCompanyInstallDvo> dvo);
+
+    PagingResult<SearchRes> mapAllDvoToSearchRes(PagingResult<WctaCompanyInstallDvo> dvo);
+
+    SaveReq mapCompanyInstallDvoToSaveReq(WctaCompanyInstallDvo dvo);
+
+    WctaCompanyInstallDvo mapSaveReqToCompanyInstallDvo(SaveReq dvo);
+
+    @Mapping(target = "cntrtZip", source = "zip")
+    @Mapping(target = "cntrtBasAdr", source = "basAdr")
+    @Mapping(target = "cntrtDtlAdr", source = "dtlAdr")
+    @Mapping(target = "istZip", source = "installZip")
+    @Mapping(target = "istBasAdr", source = "installBasAdr")
+    @Mapping(target = "istDtlAdr", source = "installDtlAdr")
+    @Mapping(target = "cntrtCralLocaraTno", source = "cralLocaraTno")
+    @Mapping(target = "cntrtMexnoEncr", source = "mexnoEncr")
+    @Mapping(target = "cntrtCralIdvTno", source = "cralIdvTno")
+    @Mapping(target = "istCralLocaraTno", source = "installCralLocaraTno")
+    @Mapping(target = "istMexnoEncr", source = "installMexnoEncr")
+    @Mapping(target = "istCralIdvTno", source = "installCralIdvTno")
+    @Mapping(target = "sellOgTpCd", source = "ogCd")
+    @Mapping(target = "istllKnm", source = "rcgvpKnm")
+    @Mapping(target = "cntrRcpDt", source = "cntrRcpDt")
+    @Mapping(target = "cntrRcpTm", source = "cntrRcpTm")
+    @Mapping(target = "cntrCnfmDt", source = "cntrRcpDt")
+    @Mapping(target = "cntrCnfmTm", source = "cntrRcpTm")
+    WctiContractCreateDvo mapCompanyInstallDvoToContractDvo(WctaCompanyInstallDvo dvo);
+
+    @Mapping(target = "copnDvCd", source = "istCopnDvCd")
+    @Mapping(target = "cralLocaraTno", source = "installCralLocaraTno")
+    @Mapping(target = "mexnoEncr", source = "installMexnoEncr")
+    @Mapping(target = "cralIdvTno", source = "installCralIdvTno")
+    @Mapping(target = "locaraTno", source = "installLocaraTno")
+    @Mapping(target = "exnoEncr", source = "installExnoEncr")
+    @Mapping(target = "idvTno", source = "installIdvTno")
+    @Mapping(target = "zip", source = "installZip")
+    @Mapping(target = "adr", source = "installBasAdr")
+    @Mapping(target = "adrDtl", source = "installDtlAdr")
+    WctaContractAdrpcBasDvo mapCompanyInstallDvoToAdrpcBasDvo(WctaCompanyInstallDvo dvo);
+
 }
--- src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/dvo/WctaCompanyInstallDvo.java
+++ src/main/java/com/kyowon/sms/wells/web/contract/ordermgmt/dvo/WctaCompanyInstallDvo.java
@@ -1,5 +1,6 @@
 package com.kyowon.sms.wells.web.contract.ordermgmt.dvo;
 
+import com.sds.sflex.system.config.annotation.DBDecField;
 import com.sds.sflex.system.config.annotation.DBEncField;
 
 import lombok.Getter;
@@ -23,9 +24,110 @@ public class WctaCompanyInstallDvo {
     private String coIstDvCd; // 설치구분
     private String coIstUswyCd; // 설치용도
     private String cralLocaraTno; // 휴대전화번호1
+    @DBDecField
     @DBEncField
     private String mexnoEncr; // 휴대전화번호2
     private String cralIdvTno; // 휴대전화번호3
     private String outputDiv; // 출력구분
     private String sellOgTpCd;// 조직유형코드
+
+    private String rowState;
+    private int dataRow;
+
+    // save data
+    //private String cntrNo;
+    //private String cntrSn;
+    private String cntrCfmn;
+    private String assignCan;
+    private String istReRegn;
+    private String cstKnm;
+    private String bryyMmdd;
+    private String bzrno;
+    private String cntrCstNo;
+    //private String rcgvpKnm;
+    private String cntrCnfmDt;
+    private String sellTpCd;
+    private String sellTpNm;
+    private String coCd;
+    private String ogCd;
+    private String coIstMngtDvCd;
+    private String coIstMngtDvNm;
+    //private String basePdCd;
+    //private String pdNm;
+    private String pkgPdCd;
+    private String pkgPdNm;
+    private String svPrd;
+    private String svPdTpCd;
+    private String svTpNm;
+    private String pdGdCd;
+    //private String coIstDvCd;
+    private String coIstDvNm;
+    //private String coIstUswyCd;
+    private String coIstUswyNm;
+    private String istPlcTpCd;
+    private String frisuBfsvcPtrmN;
+    private String frisuAsPtrmN;
+
+    private String sppDuedt;
+    private String istDt;
+    private String rtnDt;
+    private String istAkArtcMoCn;
+    private String cttRsCd;
+    private String cttPsicId;
+    private String sconCn;
+    private String sellTpDtlCd;
+    private String sellTpDtlNm;
+    private String cntrNo216;
+    private String cntrSn216;
+    private String reguDelYn;
+    private String memExpGbn;
+    private String fstRgstDt;
+    private String fstRgstTm;
+    private String fstRgstUsrId;
+    private String fstRgstUsrNm;
+    private String fnlMdfcDt;
+    private String fnlMdfcTm;
+    private String fnlMdfcUsrId;
+    private String fnlMdfcUsrNm;
+    private String copnDvCd;
+    private String mpno;
+    //private String cralLocaraTno;
+    //private String mexnoEncr;
+    //private String cralIdvTn;
+    private String zip;
+    private String basAdr;
+    private String dtlAdr;
+
+    private String installMpno;
+    private String installCralLocaraTno;
+    @DBDecField
+    private String installMexnoEncr;
+    private String installCralIdvTno;
+    private String installZip;
+    private String installBasAdr;
+    private String installDtlAdr;
+
+    private String cntrCnfmTm;
+    private String cntrRcpDt;
+    private String cntrRcpTm;
+    private String fnlAmt;
+
+    private String gender;
+    private String telNoYn;
+    private String telNo;
+    private String istCopnDvCd;
+    private String istBzrno;
+    private String installTelNoYn;
+    private String installTelNo;
+
+    private String installLocaraTno;
+    @DBDecField
+    private String installExnoEncr;
+    private String installIdvTno;
+
+    /// 삭제
+    // private String wpDvNm;
+    //private String filterExp;
+    //private String dscRate;
+    //private String cpsDt;
 }
